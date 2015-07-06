263c3fd55bb08d860d3719a3b7636715f755ba16
Ian Spielman <ian.spielman@nist.gov>
2015-07-06 15:36:03 -0400

    Fixed bug where updating a repname deleted the path information.

--- file_utils.py
+++ file_utils.py
@@ -16,7 +16,7 @@ def is_rep_name(h5_filepath):
     Decides if a filename is a repeating filename and if so
     returns true along with the basename and the index
     """
-    basename = os.path.basename(h5_filepath).split('.h5')[0]
+    path, basename = os.path.split(h5_filepath).split('.h5')[0]
     
     chunks = basename.split('_rep')
 
@@ -31,18 +31,21 @@ def is_rep_name(h5_filepath):
         return (False, basename, 0)
     else:
         basename = basename.split('_rep')[-2]
-        return (True, basename, reps)
+        return (True, path, basename, reps)
 
 def new_rep_name(h5_filepath, repeats=0):
     """
     Create a new repeating file name
     """
     
-    (isRep, basename, reps) = is_rep_name(h5_filepath)
+    (isRep, path, basename, reps) = is_rep_name(h5_filepath)
         
     if isRep:
         reps += 1
         if repeats > 1:
             reps %= repeats
-        return basename + '_rep%05d.h5'% reps
-    return basename + '_rep%05d.h5'%1
+    else:
+        reps = 1
+        
+    repname = basename + '_rep%05d.h5'% reps
+    return os.path.join(path, repname)
