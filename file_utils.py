bf985bd72aea220f817a2ba449e377b5db9bb6ce
Ian Spielman <ian.spielman@nist.gov>
2015-07-08 17:44:07 -0400

    Found some silly bugs in code making rep names.

--- file_utils.py
+++ file_utils.py
@@ -16,19 +16,20 @@ def is_rep_name(h5_filepath):
     Decides if a filename is a repeating filename and if so
     returns true along with the basename and the index
     """
-    path, basename = os.path.split(h5_filepath).split('.h5')[0]
+    path, basename = os.path.split(h5_filepath)
+    basename = basename.split('.h5')[0] 
     
     chunks = basename.split('_rep')
 
     # '_rep' not found    
     if len(chunks) == 1:
-        return (False, basename, 0)
+        return (False, path, basename, 0)
     
     try:
         reps = int(basename.split('_rep')[-1])
     except:
         # tail not an integer
-        return (False, basename, 0)
+        return (False, path, basename, 0)
     else:
         basename = basename.split('_rep')[-2]
         return (True, path, basename, reps)
